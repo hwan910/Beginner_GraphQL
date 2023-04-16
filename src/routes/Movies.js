e04aa4d3d2f934215672b8bf0fc0f75eeaa3fec3
hwan910 <psh5575@gmail.com>
2023-04-16 23:59:07 +0900

    hook 사용 안하고 graphQL로 데이터 요청

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,5 +1,31 @@
+import { gql, useApolloClient } from "@apollo/client";
+import { useEffect, useState } from "react";
+
 const Movies = () => {
-  return <div>This is a list of movies</div>;
+  const [movies, setMovies] = useState([]);
+  const client = useApolloClient();
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            allMovies {
+              title
+              id
+            }
+          }
+        `,
+      })
+      .then((result) => setMovies(result.data.allMovies));
+  }, [client]);
+
+  return (
+    <ul>
+      {movies.map((movie) => {
+        return <li key={movie.id}>{movie.title}</li>;
+      })}
+    </ul>
+  );
 };
 
 export default Movies;
